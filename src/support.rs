f717f09b0c3f5c4ee8f024c8aa81d73e5a4759e3
Valentin Lorentz <progval+git@progval.net>
2018-06-24 20:49:14 +0200

    Parameterize all types with 'input.

--- src/support.rs
+++ src/support.rs
@@ -1,10 +1,17 @@
 #[derive(Debug, PartialEq, Default)]
-pub struct token(String);
+pub struct token<'input>(&'input str);
+
 #[derive(Debug, PartialEq, Default)]
-pub struct NMTOKEN(String);
+pub struct NMTOKEN<'input>(&'input str);
+
 #[derive(Debug, PartialEq, Default)]
-pub struct QName(String);
+pub struct QName<'input>(&'input str);
+
 #[derive(Debug, PartialEq, Default)]
-pub struct anyURI(String);
+pub struct anyURI<'input>(&'input str);
+
 #[derive(Debug, PartialEq, Default)]
-pub struct nonNegativeInteger(String);
+pub struct nonNegativeInteger<'input>(&'input str);
+
+#[derive(Debug, PartialEq, Default)]
+pub struct SUPPORT_ANY<'input>(&'input str);
